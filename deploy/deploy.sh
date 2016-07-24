2e59ae23b60f1b12c4ff3ecf4a71de2d6e5da83d
Mark Cafaro <mark.cafaro@gmail.com>
2016-07-24 00:30:43 -0700

    Added AWS Elastic Beanstalk configuration files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -38,7 +38,7 @@ sed -i='' "s/<NAME>/$NAME/" Dockerrun.aws.json
 sed -i='' "s/<TAG>/$VERSION/" Dockerrun.aws.json
 
 # Zip up the Dockerrun file (feel free to zip up an .ebextensions directory with it)
-zip -r $ZIP Dockerrun.aws.json
+zip -r $ZIP Dockerrun.aws.json .ebextensions
 
 aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
 
