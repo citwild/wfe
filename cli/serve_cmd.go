6e2508a5e4e532843684547cce0fa5c14e76196f
Mark Cafaro <mark.cafaro@gmail.com>
2016-07-27 20:35:02 -0700

    Implemented HTTP

--- cli/serve_cmd.go
+++ cli/serve_cmd.go
@@ -11,7 +11,9 @@ import (
 	"github.com/citwild/wfe/cli/internal/middleware"
 	"github.com/citwild/wfe/service"
 	"github.com/citwild/wfe/store/localstore"
+	"github.com/soheilhy/cmux"
 	"google.golang.org/grpc"
+	"net"
 	"sourcegraph.com/sourcegraph/go-flags"
 )
 
@@ -28,22 +30,72 @@ func init() {
 }
 
 type ServeCmd struct {
+	HTTPAddr  string `long:"http-addr" default:":8080" description:"HTTP listen address for app and gRPC API" env:"WFE_HTTP_ADDR"`
 	HTTPSAddr string `long:"https-addr" default:":8443" description:"HTTPS (TLS) listen address for app and gRPC API" env:"WFE_HTTPS_ADDR"`
 
-	CertFile string `long:"tls-cert" description:"certificate file for TLS" env:"WFE_TLS_CERT" required:"yes"`
-	KeyFile  string `long:"tls-key" description:"key file for TLS" env:"WFE_TLS_KEY" required:"yes"`
+	CertFile string `long:"tls-cert" description:"certificate file for TLS" env:"WFE_TLS_CERT"`
+	KeyFile  string `long:"tls-key" description:"key file for TLS" env:"WFE_TLS_KEY"`
 }
 
 func (c *ServeCmd) Execute(_ []string) error {
-	err := serveHTTPS(c.HTTPSAddr, c.CertFile, c.KeyFile)
+	// gRPC API
+	grpcConfig := &grpcConfig{}
+	grpcConfig.servers = service.NewServers()
+	inj := middleware.NewInjector(grpcConfig.servers, localstore.NewStores())
+	grpcConfig.opts = []grpc.ServerOption{grpc.UnaryInterceptor(inj.Inject)}
+
+	// web app
+	httpHandler := http.NewServeMux()
+	httpHandler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Not yet implemented")
+	})
+
+	err := serveHTTP(c.HTTPAddr, grpcConfig, httpHandler)
 	if err != nil {
 		return err
+	}
+
+	useTLS := c.CertFile != "" || c.KeyFile != ""
+	if useTLS {
+		err = serveHTTPS(c.HTTPSAddr, grpcConfig, httpHandler, c.CertFile, c.KeyFile)
+		if err != nil {
+			return err
+		}
 	}
 
 	select {}
 }
 
-func serveHTTPS(addr string, certFile string, keyFile string) error {
+func serveHTTP(addr string, grpcConfig *grpcConfig, httpHandler http.Handler) error {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	// main multiplexer
+	mux := cmux.New(lis)
+
+	// gRPC API
+	grpcSrv := api.NewServer(grpcConfig.servers, grpcConfig.opts...)
+
+	// web app
+	httpSrv := &http.Server{}
+	httpSrv.Addr = addr
+	httpSrv.Handler = httpHandler
+
+	// multiplex connection between gRPC API and app
+	grpcLis := mux.Match(cmux.HTTP2HeaderField("content-type", "application/grpc"))
+	httpLis := mux.Match(cmux.Any())
+
+	log.Print("HTTP running on ", addr)
+	go func() { log.Fatal(grpcSrv.Serve(grpcLis)) }()
+	go func() { log.Fatal(httpSrv.Serve(httpLis)) }()
+	go func() { log.Fatal(mux.Serve()) }()
+
+	return nil
+}
+
+func serveHTTPS(addr string, grpcConfig *grpcConfig, httpHandler http.Handler, certFile string, keyFile string) error {
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		return err
@@ -61,23 +113,14 @@ func serveHTTPS(addr string, certFile string, keyFile string) error {
 	srv.TLSConfig = config
 
 	// gRPC API
-	srvs := service.NewServers()
-	strs := localstore.NewStores()
-	inj := middleware.NewInjector(srvs, strs)
-	grpcSrv := api.NewServer(srvs, grpc.UnaryInterceptor(inj.Inject))
-
-	// web app
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Not yet implemented")
-	})
+	grpcSrv := api.NewServer(grpcConfig.servers, grpcConfig.opts...)
 
 	// multiplex connection between gRPC API and app
 	srv.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcSrv.ServeHTTP(w, r)
 		} else {
-			mux.ServeHTTP(w, r)
+			httpHandler.ServeHTTP(w, r)
 		}
 	})
 
@@ -85,4 +128,9 @@ func serveHTTPS(addr string, certFile string, keyFile string) error {
 	go func() { log.Fatal(srv.Serve(lis)) }()
 
 	return nil
+}
+
+type grpcConfig struct {
+	servers api.Servers
+	opts    []grpc.ServerOption
 }
